1b1a1961bced88452a594d90ccb13fa2641566ec
Julien Valverdé <julien.valverde@mailo.com>
2025-02-17 00:16:41 +0100

    Dependencies upgrade

--- packages/example/src/routes/tests.tsx
+++ packages/example/src/routes/tests.tsx
@@ -1,6 +1,5 @@
 import { R } from "@/reffuse"
 import { createFileRoute } from "@tanstack/react-router"
-import { GetRandomValues, makeUuid4 } from "@typed/id"
 import { Console, Effect } from "effect"
 
 
@@ -9,15 +8,16 @@ export const Route = createFileRoute("/tests")({
 })
 
 function RouteComponent() {
-    // R.useMemo(Effect.addFinalizer(() => Console.log("Cleanup!")).pipe(
-    //     Effect.map(() => "test")
-    // ))
+    // const value = R.useMemoScoped(Effect.addFinalizer(() => Console.log("cleanup")).pipe(
+    //     Effect.andThen(makeUuid4),
+    //     Effect.provide(GetRandomValues.CryptoRandom),
+    // ), [])
+    // console.log(value)
 
-    const value = R.useMemoScoped(Effect.addFinalizer(() => Console.log("cleanup")).pipe(
-        Effect.andThen(makeUuid4),
-        Effect.provide(GetRandomValues.CryptoRandom),
-    ), [])
-    console.log(value)
+    R.useFork(Effect.addFinalizer(() => Console.log("cleanup")).pipe(
+        Effect.andThen(Console.log("ouient")),
+        Effect.delay("1 second"),
+    ))
 
     return <div>Hello "/tests"!</div>
 }
