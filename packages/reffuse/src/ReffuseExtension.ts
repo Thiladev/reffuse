c0097bbe8128a49aec21da6fd4703a18e7034362
Julien Valverdé <julien.valverde@mailo.com>
2025-02-20 14:57:46 +0100

    Extension tests

--- packages/reffuse/src/ReffuseExtension.ts
+++ packages/reffuse/src/ReffuseExtension.ts
@@ -1,1 +1,19 @@
-export const make = <R>() => undefined
+import type { Effect } from "effect"
+import * as ReffuseContext from "./ReffuseContext.js"
+
+
+interface ReffuseTest<R> {
+    readonly contexts: readonly ReffuseContext.ReffuseContext<R>[]
+
+    useEffect<A, E>(effect: Effect.Effect<A, E, R>): void
+}
+
+const ReffuseTestProto = {
+    useEffect<A, E, R>(this: ReffuseTest<R>, effect: Effect.Effect<A, E>) {}
+}
+
+const make = (): ReffuseTest<never> => {
+    const self = Object.create(ReffuseTestProto)
+    self.contexts = []
+    return self
+}
