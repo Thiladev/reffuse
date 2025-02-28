3f2639fda1e5ffa85e09553700571b4dac2d527e
Julien Valverdé <julien.valverde@mailo.com>
2025-02-28 16:08:08 +0100

    Query work

--- packages/extension-query/src/index.ts
+++ packages/extension-query/src/index.ts
@@ -15,6 +15,24 @@ export interface UseQueryResult<A, E> {
 }
 
 
+const interruptRunningQuery = (fiberRef: Ref.Ref<Option.Option<Fiber.Fiber<void>>>) => fiberRef.pipe(
+    Effect.flatMap(Option.match({
+        onSome: Fiber.interrupt,
+        onNone: () => Effect.void,
+    }))
+)
+
+const runQuery = <A, E, R>(
+    effect: Effect.Effect<A, E, R | Scope.Scope>,
+    stateRef: Ref.Ref<AsyncData.AsyncData<A, E>>,
+) => effect.pipe(
+    Effect.matchCauseEffect({
+        onSuccess: v => Ref.set(stateRef, AsyncData.success(v)),
+        onFailure: c => Ref.set(stateRef, AsyncData.failure(c)),
+    })
+)
+
+
 export const QueryExtension = ReffuseExtension.make(() => ({
     useQuery<A, E, R>(
         this: ReffuseHelpers.ReffuseHelpers<R>,
@@ -22,45 +40,31 @@ export const QueryExtension = ReffuseExtension.make(() => ({
     ): UseQueryResult<A, E> {
         const context = this.useContext()
 
-        const fiberRef = this.useRef(Option.none<Fiber.Fiber<void, never>>())
+        const fiberRef = this.useRef(Option.none<Fiber.Fiber<void>>())
         const stateRef = this.useRef(AsyncData.noData<A, E>())
 
-        const interruptRunningQuery = React.useMemo(() => fiberRef.pipe(
-            Effect.flatMap(Option.match({
-                onSome: Fiber.interrupt,
-                onNone: () => Effect.void,
-            }))
-        ), [fiberRef])
-
-        const runQuery = React.useMemo(() => props.effect().pipe(
-            Effect.matchCauseEffect({
-                onSuccess: v => Ref.set(stateRef, AsyncData.success(v)),
-                onFailure: c => Ref.set(stateRef, AsyncData.failure(c)),
-            })
-        ), [...props.deps, stateRef])
-
-        const triggerRefresh = React.useMemo(() => interruptRunningQuery.pipe(
+        const triggerRefresh = React.useMemo(() => interruptRunningQuery(fiberRef).pipe(
             Effect.andThen(Ref.update(stateRef, prev =>
                 AsyncData.isSuccess(prev) || AsyncData.isFailure(prev)
                     ? AsyncData.refreshing(prev)
                     : AsyncData.loading()
             )),
-            Effect.andThen(runQuery),
+            Effect.andThen(runQuery(props.effect(), stateRef)),
             Effect.provide(context),
             Effect.scoped,
             Effect.forkDaemon,
 
             Effect.flatMap(fiber => Ref.set(fiberRef, Option.some(fiber))),
-        ), [interruptRunningQuery, stateRef, runQuery, context, fiberRef])
+        ), [stateRef, context, fiberRef])
 
-        this.useEffect(() => interruptRunningQuery.pipe(
+        this.useEffect(() => interruptRunningQuery(fiberRef).pipe(
             Effect.andThen(Ref.set(stateRef, AsyncData.loading())),
-            Effect.andThen(runQuery),
+            Effect.andThen(runQuery(props.effect(), stateRef)),
             Effect.scoped,
             Effect.forkDaemon,
 
             Effect.flatMap(fiber => Ref.set(fiberRef, Option.some(fiber))),
-        ), [stateRef, runQuery, fiberRef])
+        ), [stateRef, fiberRef])
 
         return React.useMemo(() => ({ state: stateRef, triggerRefresh }), [stateRef, triggerRefresh])
     }
