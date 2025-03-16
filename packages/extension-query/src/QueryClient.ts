1f0ff725ff7d4c6ed9f1ea7f4db33f96d03aad02
Julien Valverdé <julien.valverde@mailo.com>
2025-03-16 04:05:39 +0100

    Fix

--- packages/extension-query/src/QueryClient.ts
+++ packages/extension-query/src/QueryClient.ts
@@ -20,34 +20,20 @@ export interface ServiceProps<EH, HandledE> {
 }
 
 export interface ServiceResult<Self, EH, HandledE> extends Context.TagClass<Self, typeof id, QueryClient<EH, HandledE>> {
-    readonly Live: Layer.Layer<
-        | Self
-        | (EH extends ErrorHandler.DefaultErrorHandler
-            ? ErrorHandler.DefaultErrorHandler
-            : never)
-    >
+    readonly Live: Layer.Layer<Self>
 }
 
 export const Service = <
     EH = ErrorHandler.DefaultErrorHandler,
-    HandledE = never,
+    HandledE = ErrorHandler.Error<Context.Tag.Service<ErrorHandler.DefaultErrorHandler>>,
 >(
     props?: ServiceProps<EH, HandledE>
 ) => (
     <Self>(): ServiceResult<Self, EH, HandledE> => {
         const TagClass = Context.Tag(id)() as ServiceResult<Self, EH, HandledE>
-        (TagClass as Mutable<typeof TagClass>).Live = Layer.empty.pipe(
-            Layer.provideMerge(
-                Layer.succeed(TagClass, {
-                    ErrorHandler: (props?.ErrorHandler ?? ErrorHandler.DefaultErrorHandler) as Context.Tag<EH, ErrorHandler.ErrorHandler<HandledE>>
-                })
-            ),
-
-            Layer.provideMerge((props?.ErrorHandler
-                ? Layer.empty
-                : ErrorHandler.DefaultErrorHandler.Live
-            ) as Layer.Layer<ErrorHandler.DefaultErrorHandler>),
-        )
+        (TagClass as Mutable<typeof TagClass>).Live = Layer.succeed(TagClass, {
+            ErrorHandler: (props?.ErrorHandler ?? ErrorHandler.DefaultErrorHandler) as Context.Tag<EH, ErrorHandler.ErrorHandler<HandledE>>
+        })
         return TagClass
     }
 )
