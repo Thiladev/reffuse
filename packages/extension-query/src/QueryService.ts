b04860aa25a1f2b2d6a504baa27372439e0408ea
Julien Valverdé <julien.valverde@mailo.com>
2025-03-16 04:32:51 +0100

    Cleanup

--- packages/extension-query/src/QueryService.ts
+++ packages/extension-query/src/QueryService.ts
@@ -11,22 +11,3 @@ export interface QueryService<K extends readonly unknown[], A, E> {
 export const Tag = <const Id extends string>(id: Id) => <
     Self, K extends readonly unknown[], A, E = never,
 >() => Effect.Tag(id)<Self, QueryService<K, A, E>>()
-
-
-// export interface LayerProps<A, E, R> {
-//     readonly query: Effect.Effect<A, E, R>
-// }
-
-// export const layer = <Self, Id extends string, A, E, R>(
-//     tag: Context.TagClass<Self, Id, QueryService<A, E>>,
-//     props: LayerProps<A, E, R>,
-// ): Layer.Layer<Self, never, R> => Layer.effect(tag, Effect.gen(function*() {
-//     const runner = yield* QueryRunner.make({
-//         query: props.query
-//     })
-
-//     return {
-//         state: runner.stateRef,
-//         refresh: runner.forkRefresh,
-//     }
-// }))
