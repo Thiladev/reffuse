8c0d6b4c8ac2c67902baa490cb1161375b40830d
Julien Valverdé <julien.valverde@mailo.com>
2025-03-21 01:24:52 +0100

    Cleanup

--- packages/extension-query/src/internal/index.ts
+++ packages/extension-query/src/internal/index.ts
@@ -1,6 +1,3 @@
-/** @internal */
 export * as MutationRunner from "./MutationRunner.js"
-/** @internal */
 export * as QueryRunner from "./QueryRunner.js"
-/** @internal */
 export * as QueryState from "./QueryState.js"
