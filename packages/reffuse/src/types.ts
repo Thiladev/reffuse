6e8ce84851b4f766b5a2e42552ec2ec36e06ecd8
Julien Valverdé <julien.valverde@mailo.com>
2025-04-12 23:06:43 +0200

    Cleanup

--- packages/reffuse/src/types.ts
+++ packages/reffuse/src/types.ts
@@ -8,14 +8,4 @@ export type CommonKeys<A, B> = Extract<keyof A, keyof B>
  */
 export type StaticType<T extends abstract new (...args: any) => any> = Omit<T, "prototype">
 
-export type Extend<Super, Self> =
-    Extendable<Super, Self> extends true
-        ? Omit<Super, CommonKeys<Self, Super>> & Self
-        : never
-
-export type Extendable<Super, Self> =
-    Pick<Self, CommonKeys<Self, Super>> extends Pick<Super, CommonKeys<Self, Super>>
-        ? true
-        : false
-
 export type Merge<Super, Self> = Omit<Super, CommonKeys<Self, Super>> & Self
