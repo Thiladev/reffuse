fc4295894f69e39a79b96e3196198106349afc34
Julien Valverdé <julien.valverde@mailo.com>
2025-04-16 00:44:01 +0200

    Form work

--- packages/extension-form/src/internal/FormField.ts
+++ packages/extension-form/src/internal/FormField.ts
@@ -1,25 +1,31 @@
 import type { Schema } from "effect"
-import type * as Form from "./Form.ts"
+import type * as FormTree from "./FormTree.ts"
 
 
 export interface FormField<S extends Schema.Schema.Any> {
-
+    readonly schema: S
 }
 
 export interface GenericFormField<S extends Schema.Schema.Any> extends FormField<S> {
     readonly _tag: "GenericFormField"
-    readonly schema: S
     readonly value: S["Type"]
 }
 
-export interface ArrayFormField<S extends Schema.Array$<Schema.Schema.Any>> extends FormField<S> {
-    readonly _tag: "ArrayFormField"
-    readonly schema: S
-    readonly elements: readonly Form.FormTree<S["value"]>[]
+export interface TupleFormField<S extends Schema.Tuple<readonly Schema.Schema.AnyNoContext[]>> extends FormField<S> {
+    readonly _tag: "TupleFormField"
+    readonly elements: { readonly [K in keyof S["elements"]]: FormTree.FormTree<S["elements"][K]> }
 }
 
-export interface StructFormField<S extends Schema.Struct<Schema.Struct.Fields>> extends FormField<S> {
+export interface ArrayFormField<S extends Schema.Array$<Schema.Schema.AnyNoContext>> extends FormField<S> {
+    readonly _tag: "ArrayFormField"
+    readonly elements: readonly FormTree.FormTree<S["value"]>[]
+}
+
+export interface StructFormField<S extends Schema.Struct<{
+    readonly [x: string]: Schema.Schema.AnyNoContext
+    readonly [x: number]: Schema.Schema.AnyNoContext
+    readonly [x: symbol]: Schema.Schema.AnyNoContext
+}>> extends FormField<S> {
     readonly _tag: "StructFormField"
-    readonly schema: S
-    readonly fields: { [K in keyof S["fields"]]: Form.FormTree<S["fields"][K]> }
+    readonly fields: { [K in keyof S["fields"]]: FormTree.FormTree<S["fields"][K]> }
 }
---
+++ packages/extension-form/src/internal/FormTree.ts
@@ -0,0 +1,21 @@
+import { Schema } from "effect"
+import type * as FormField from "./FormField.js"
+
+
+export type FormTree<S extends Schema.Schema<any>> = (
+    S extends Schema.Array$<any> ? FormField.ArrayFormField<S> :
+    S extends Schema.Struct<any> ? FormField.StructFormField<S> :
+    FormField.GenericFormField<S>
+)
+
+
+const MySchema = Schema.Struct({
+    name: Schema.String,
+    symbol: Schema.SymbolFromSelf,
+    values: Schema.Array(Schema.String),
+})
+
+type TestFormTree = FormTree<typeof MySchema>
+declare const testFormTree: TestFormTree
+
+testFormTree.fields.values.elements
