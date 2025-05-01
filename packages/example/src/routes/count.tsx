aa6c4a8008e77de8c225ae540d16ab0d5e58e6c2
Julien Valverdé <julien.valverde@mailo.com>
2025-05-01 16:19:42 +0200

    Fix

--- packages/example/src/routes/count.tsx
+++ packages/example/src/routes/count.tsx
@@ -12,13 +12,12 @@ function Count() {
     const runSync = R.useRunSync()
 
     const countRef = R.useRef(() => Effect.succeed(0))
-    const [count] = R.useRefState(countRef)
+    const [count] = R.useSubscribeRefs(countRef)
 
 
     return (
         <div className="container mx-auto">
-            {/* <button onClick={() => setCount((count) => count + 1)}> */}
-            <button onClick={() => Ref.update(countRef, count => count + 1).pipe(runSync)}>
+            <button onClick={() => runSync(Ref.update(countRef, count => count + 1))}>
                 count is {count}
             </button>
         </div>
