1c99d5c161c5fb39ca25743f6f68be0a6061b065
Julien Valverdé <julien.valverde@mailo.com>
2025-05-23 03:34:02 +0200

    QueryService

--- packages/extension-query/src/QueryService.ts
+++ packages/extension-query/src/QueryService.ts
@@ -3,15 +3,9 @@ import { type Cause, Effect, type Fiber, type Option, type Stream, type Subscrip
 import * as QueryRunner from "./QueryRunner.js"
 
 
-export interface QueryService<K extends readonly unknown[], A, E> {
-    readonly latestKey: SubscriptionRef.SubscriptionRef<Option.Option<K>>
-    readonly state: SubscriptionRef.SubscriptionRef<AsyncData.AsyncData<A, E>>
-    readonly forkRefresh: Effect.Effect<readonly [
-        fiber: Fiber.RuntimeFiber<AsyncData.Success<A> | AsyncData.Failure<E>, Cause.NoSuchElementException>,
-        state: Stream.Stream<AsyncData.AsyncData<A, E>>,
-    ]>
-}
-
 export const Tag = <const Id extends string>(id: Id) => <
     Self, K extends readonly unknown[], A, E = never
 >() => Effect.Tag(id)<Self, QueryRunner.QueryRunner<K, A, E>>()
+
+
+export const Service = <Self>() =>
