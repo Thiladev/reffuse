f25fefb0f312a8882274acea8f65ef5f1b616ae4
Julien Valverdé <julien.valverde@mailo.com>
2025-05-23 17:33:19 +0200

    Cleanup

--- packages/extension-query/src/QueryService.ts
+++ packages/extension-query/src/QueryService.ts
@@ -1,24 +1,7 @@
-import type * as AsyncData from "@typed/async-data"
-import { type Cause, Effect, type Fiber, type Option, type Stream, type SubscriptionRef } from "effect"
+import { Effect } from "effect"
 import * as QueryRunner from "./QueryRunner.js"
-import * as QueryProgress from "./QueryProgress.js"
 
 
 export const Tag = <const Id extends string>(id: Id) => <
     Self, K extends readonly unknown[], A, E = never
 >() => Effect.Tag(id)<Self, QueryRunner.QueryRunner<K, A, E>>()
-
-
-export interface ServiceProps<K extends readonly unknown[], A, E, R> {
-    readonly key: Stream.Stream<K>
-    readonly query: (key: K) => Effect.Effect<A, E, R | QueryProgress.QueryProgress>
-    readonly options?: QueryRunner.RunOptions
-}
-
-export const Service = <Self>() => <
-    const Id extends string,
-    K extends readonly unknown[], A, E, R,
->(
-    id: Id,
-    props: ServiceProps<K, A, E, R>,
-) =>
