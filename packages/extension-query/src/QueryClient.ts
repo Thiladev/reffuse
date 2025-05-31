f4e380ddcb675d9814aae2129aec3cc89a86ef90
Julien Valverdé <julien.valverde@mailo.com>
2025-05-31 05:57:39 +0200

    QueryClient refactoring work

--- packages/extension-query/src/QueryClient.ts
+++ packages/extension-query/src/QueryClient.ts
@@ -8,6 +8,17 @@ export interface QueryClient<FallbackA, HandledE> {
 }
 
 
+export interface MakeProps<FallbackA, HandledE, E, R> {
+    readonly errorHandler: Effect.Effect<QueryErrorHandler.QueryErrorHandler<FallbackA, HandledE>, E, R>
+}
+
+export const make = <FallbackA, HandledE, E, R>(
+    { errorHandler }: MakeProps<FallbackA, HandledE, E, R>
+): Effect.Effect<QueryClient<FallbackA, HandledE>, E, R> => Effect.Do.pipe(
+    Effect.bind("errorHandler", () => errorHandler)
+)
+
+
 const id = "@reffuse/extension-query/QueryClient"
 
 export type TagClassShape<FallbackA, HandledE> = Context.TagClassShape<typeof id, QueryClient<FallbackA, HandledE>>
@@ -19,29 +30,26 @@ export type GenericTagClass<FallbackA, HandledE> = Context.TagClass<
 export const makeGenericTagClass = <FallbackA = never, HandledE = never>(): GenericTagClass<FallbackA, HandledE> => Context.Tag(id)()
 
 
-export interface ServiceProps<EH, FallbackA, HandledE> {
-    readonly ErrorHandler?: Context.Tag<EH, QueryErrorHandler.QueryErrorHandler<FallbackA, HandledE>>
+export interface ServiceProps<FallbackA = never, HandledE = never, E = never, R = never> {
+    readonly errorHandler?: Effect.Effect<QueryErrorHandler.QueryErrorHandler<FallbackA, HandledE>, E, R>
 }
 
-export interface ServiceResult<Self, EH, FallbackA, HandledE> extends Context.TagClass<
+export interface ServiceResult<Self, FallbackA, HandledE, E, R> extends Context.TagClass<
     Self,
     typeof id,
     QueryClient<FallbackA, HandledE>
 > {
-    readonly Default: Layer.Layer<
-        Self | (EH extends QueryErrorHandler.DefaultQueryErrorHandler ? EH : never),
-        never,
-        EH extends QueryErrorHandler.DefaultQueryErrorHandler ? never : EH
-    >
+    readonly Default: Layer.Layer<QueryErrorHandler.QueryErrorHandler<FallbackA, HandledE>, E, R>
 }
 
 export const Service = <Self>() => (
     <
-        EH = QueryErrorHandler.DefaultQueryErrorHandler,
         FallbackA = QueryErrorHandler.Fallback<Context.Tag.Service<typeof QueryErrorHandler.DefaultQueryErrorHandler>>,
         HandledE = QueryErrorHandler.Error<Context.Tag.Service<typeof QueryErrorHandler.DefaultQueryErrorHandler>>,
+        E = never,
+        R = never,
     >(
-        props?: ServiceProps<EH, FallbackA, HandledE>
+        props?: ServiceProps<FallbackA, HandledE, E, R>
     ): ServiceResult<Self, EH, FallbackA, HandledE> => {
         const TagClass = Context.Tag(id)() as ServiceResult<Self, EH, FallbackA, HandledE>
 
